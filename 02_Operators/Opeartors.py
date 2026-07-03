85c8f721654327b0f61a460a16f1f28b7098e531
Vikrant kumar <vikrant010208@gmail.com>
2026-07-03 08:56:48 +0530

    operator in python

---
+++ 02_Operators/Opeartors.py
@@ -0,0 +1,17 @@
+a = 4
+b = 5
+
+# Addition
+print("The sum is: " ,a+b)
+
+# Subtraction
+print("The difference is: " ,a-b)
+
+# Multiplication
+print("The multiplication is: " ,a*b)
+
+# Division
+print("The division is: " ,a/b)
+
+# Modulus
+print("The modulus is: " ,a%b)
