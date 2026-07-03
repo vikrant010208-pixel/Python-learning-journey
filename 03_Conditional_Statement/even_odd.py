f245ce202d4c615589c1f392c06d2658f35ce14d
Vikrant kumar <vikrant010208@gmail.com>
2026-07-03 09:07:57 +0530

    Add python basic file to practice

---
+++ 03_Conditional_Statement/even_odd.py
@@ -0,0 +1,8 @@
+# Taking input from the user.
+num = int(input("Enter the number here to check: "))
+
+# Check the conditions.
+if num % 2 == 0:
+    print("The given number is even")
+else:
+    print("The given number is odd")
