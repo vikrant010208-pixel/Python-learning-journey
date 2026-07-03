775d64f3b08097379b406bfcea7dc5454ab3f9c0
Vikrant kumar <vikrant010208@gmail.com>
2026-07-03 09:16:26 +0530

    Add python basic exercise to practice

---
+++ 03_Conditional_Statement/Check_pass_fail_sysytem.py
@@ -0,0 +1,8 @@
+# Taking input from user.
+marks = int(input("Enter your marks to check: "))
+
+# Conditions to check
+if marks <= 33:
+    print("you are Fail.")
+else:
+    print("You are passed.")
