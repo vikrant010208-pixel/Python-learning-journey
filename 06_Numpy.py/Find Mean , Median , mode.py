d248647e97f854cd69cb773f39df947a7ec673a0
Vikrant kumar <vikrant010208@gmail.com>
2026-07-15 10:40:44 +0530

    Add some basics of  numpy library

---
+++ 06_Numpy.py/Find Mean , Median , mode.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data = np.array([10,20,30,40,50])
+
+# Find Mean(Total sum)
+print("Total Sum: " ,np.sum(data))
+
+# Find Median(Average)
+print("Average (Median): " ,np.mean(data))
+
+# Find mode(Max. Value)
+print("Max value: " ,np.max(data))
