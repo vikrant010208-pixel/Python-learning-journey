ae832b3a1fbc1e8dc6923045c4b8c71a1cd94559
Vikrant kumar <vikrant010208@gmail.com>
2026-07-15 10:32:48 +0530

    Add some basics of numpy library

---
+++ 06_Numpy.py/Opeartion.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([5,6,7,8])
+
+# Addition 
+print("Addition: " ,a+b)
+
+# Multiplication
+print("Multiplication: " ,a*b)
+
+# Subtraction
+print("Subtraction: " ,a-b)
+
+# Division
+print("Division: " ,a/b)
