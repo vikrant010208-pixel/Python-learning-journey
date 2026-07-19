0f3f3ce9c8e8545723ae6bf7cc61052d9f3930cc
Vikrant kumar <vikrant010208@gmail.com>
2026-07-19 10:16:42 +0530

    Add basics

--- 08_Matploylib/Create Bar chart.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-cities = ['Delhi', 'Mumbai', 'Pune', 'Bangalore']
-population = [30, 30, 10, 15] # In millions
-
-# Replace plot() from bar()
-plt.bar(cities, population, color = ['red', 'blue', 'green', 'yellow'])
-plt.title("City population (In millions)")
-plt.show()
