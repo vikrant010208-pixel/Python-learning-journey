f0183ee6b44af44fa6dab768ca9938dfa53f5a65
Vikrant kumar <vikrant010208@gmail.com>
2026-09-03 17:48:26 +0530

    Add a mini calculator

--- 03_Conditional_Statement/Mini Calculator.py
+++ 03_Conditional_Statement/Mini Calculator.py
@@ -1,30 +1,40 @@
 print("===== WELCOME TO MINI CALCULATOR =====")
 
-# Taking input 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+while True:
+    # Taking input
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-# Choosing an operator.
-print("1. +")
-print("2. -")
-print("3. *")
-print("4. /")
-choice = input("\n Select an operator.")
+    # Choosing an operator
+    print("\n1. +")
+    print("2. -")
+    print("3. *")
+    print("4. /")
 
-# Perform an operation
-if choice == "1":
-    print("The addition of the two given number is: " ,num1 + num2)
+    choice = input("\nSelect an operator: ")
 
-elif choice == "2":
-    print("The subtraction of the two given number is: " ,num1 - num2)
+    # Perform an operation
+    if choice == "1":
+        print("The addition of the two given numbers is:", num1 + num2)
 
-elif choice == "3":
-    print("The multiplication of the given two number is: " , num1 * num2)
+    elif choice == "2":
+        print("The subtraction of the two given numbers is:", num1 - num2)
 
-elif choice == "4":
-    try:
-        print("The division of the two given number is: " ,num1 / num2)
-    except ZeroDivisionError:
-        print("Error: Division by zero is not allowed.")
-else:
-    print("Invalid choice! Please select a valid operator from the menu.")
+    elif choice == "3":
+        print("The multiplication of the two given numbers is:", num1 * num2)
+
+    elif choice == "4":
+        try:
+            print("The division of the two given numbers is:", num1 / num2)
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed.")
+
+    else:
+        print("Invalid choice! Please select a valid operator from the menu.")
+
+    # Ask whether to continue
+    again = input("\nDo you want to continue? (Yes/No): ")
+
+    if again.strip().lower() != "yes":
+        print("Thank you for using my calculator! 😊")
+        break
